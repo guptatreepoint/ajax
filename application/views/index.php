05d0e5f37f1e44caf41cb3faabb30d6675a77f54
Sumit Gupta <guptatreepoint@gmail.com>
2021-01-16 18:16:45 +0530

    Added filter in datatabe

--- application/views/index.php
+++ application/views/index.php
@@ -123,7 +123,7 @@
 	                $('#createModal').modal('hide');
 	                $('#createForm')[0].reset();
 	                alert('Successfully inserted');
-	               $('#example1').DataTable().ajax.reload();
+	                loadDatatableAjax();
 	              },
 	           error: function()
 	           {
@@ -137,13 +137,62 @@
 
 		//displaying data on page start here
 		$(document).ready(function(){
-			$('#example1').DataTable({
-				"ajax" : "<?php echo base_url('my_controller/fetchDatafromDatabase'); ?>",
-				"order": [],
-			});
+			loadDatatableAjax();
 		});
-		//displaying data on page end here
 
+		function loadDatatableAjax(){
+			$('#example1').DataTable({
+				"bDestroy" : true,
+				"ajax" : "<?php echo base_url('my_controller/fetchDatafromDatabase'); ?>",
+				"initComplete" : function(){
+					var notApplyFilterOnColumn = [4];
+					var inputFilterOnColumn = [0];
+					var showFilterBox = 'afterHeading'; //beforeHeading, afterHeading
+					$('.gtp-dt-filter-row').remove();
+					var theadSecondRow = '<tr class="gtp-dt-filter-row">';
+					$(this).find('thead tr th').each(function(index){
+						theadSecondRow += '<td class="gtp-dt-select-filter-' + index + '"></td>';
+					});
+					theadSecondRow += '</tr>';
+
+					if(showFilterBox === 'beforeHeading'){
+						$(this).find('thead').prepend(theadSecondRow);
+					}else if(showFilterBox === 'afterHeading'){
+						$(theadSecondRow).insertAfter($(this).find('thead tr'));
+					}
+
+					this.api().columns().every( function (index) {
+						var column = this;
+
+						if(inputFilterOnColumn.indexOf(index) >= 0 && notApplyFilterOnColumn.indexOf(index) < 0){
+							$('td.gtp-dt-select-filter-' + index).html('<input type="text" class="gtp-dt-input-filter">');
+			                $( 'td input.gtp-dt-input-filter').on( 'keyup change clear', function () {
+			                    if ( column.search() !== this.value ) {
+			                        column
+			                            .search( this.value )
+			                            .draw();
+			                    }
+			                } );
+						}else if(notApplyFilterOnColumn.indexOf(index) < 0){
+							var select = $('<select><option value="">Select</option></select>')
+			                    .on( 'change', function () {
+			                        var val = $.fn.dataTable.util.escapeRegex(
+			                            $(this).val()
+			                        );
+			 
+			                        column
+			                            .search( val ? '^'+val+'$' : '', true, false )
+			                            .draw();
+			                    } );
+			                $('td.gtp-dt-select-filter-' + index).html(select);
+			                column.data().unique().sort().each( function ( d, j ) {
+			                    select.append( '<option value="'+d+'">'+d+'</option>' )
+			                } );
+						}
+					});
+				}
+			});
+		}
 
 		//edit function start here
 		function editFun(id)
@@ -187,7 +236,7 @@
 	                else{
 	                	alert('Updation Failed !');
 	                }
-	               $('#example1').DataTable().ajax.reload();
+	               loadDatatableAjax();
 	              },
 	           error: function()
 	           {
@@ -213,7 +262,7 @@
 						if(response==1)
 						{
 							alert('Data Deleted Successfully');
-							$('#example1').DataTable().ajax.reload();
+							loadDatatableAjax();
 						}
 						else
 						{
